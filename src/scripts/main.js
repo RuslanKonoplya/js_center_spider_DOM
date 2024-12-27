d720d2f2ab5733745413e3f33a764fe8b2859d66
Ruslan <ruslan.konoplya@gmail.com>
2024-12-27 13:33:30 +0200

    add sol

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,8 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-const heigth1 = wall.getBoundingClientRect().height / 2 - 50;
-const weigth1 = wall.getBoundingClientRect().width / 2 - 50;
+const heigth = wall.getBoundingClientRect().height / 2 - 50;
+const weigth = wall.getBoundingClientRect().width / 2 - 50;
 
-spider.style.top = `${heigth1}px`;
-spider.style.left = `${weigth1}px`;
+spider.style.top = `${heigth}px`;
+spider.style.left = `${weigth}px`;
