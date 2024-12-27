9243b9a80299292465be48055591f712630a7ffe
Ruslan <ruslan.konoplya@gmail.com>
2024-12-27 13:29:18 +0200

    make build

---
+++ index.73ed456e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.getBoundingClientRect().height/2-50,n=t.getBoundingClientRect().width/2-50;e.style.top="".concat(c,"px"),e.style.left="".concat(n,"px");
+//# sourceMappingURL=index.73ed456e.js.map
